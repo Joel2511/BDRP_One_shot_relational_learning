8ca09d9e2fab4b69c87ecb4846aab52fc5c0f646
Joel2511 <97803972+Joel2511@users.noreply.github.com>
2025-12-11 13:55:53 +0100

    Fix order of elements in false_pairs
    Corrected the order of elements in false_pairs to ensure it follows the format [Head, Noise].

--- One-shot-Relational-Learning/data_loader.py
+++ One-shot-Relational-Learning/data_loader.py
@@ -102,12 +102,13 @@ def train_generate(dataset, batch_size, few, symbol2id, ent2id, e1rel_e2):
                 if noise == e_t:
                     continue
                 break
-            false_pairs.append([symbol2id[noise], symbol2id[e_h]])
+            
+            # --- FIX IS HERE: Correct order is [Head, Noise] ---
+            false_pairs.append([symbol2id[e_h], symbol2id[noise]]) 
             false_left.append(ent2id[e_h])
             false_right.append(ent2id[noise])
 
         yield support_pairs, query_pairs, false_pairs, support_left, support_right, query_left, query_right, false_left, false_right
-
 
 
 def train_generate_(dataset, batch_size, few, symbol2id, ent2id, e1rel_e2, num_neg=1):
